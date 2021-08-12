15dab27c9daa18121c6e54edae6f9387a609eb20
Dawid Mróz <dmroz0707@gmail.com>
2021-08-12 22:15:50 +0200

    Calc 1.3
    Co nowego? | What's new?
    -Dodanie nowej logiki | Adding new logic
    -Dodanie obsługi liczb zmiennoprzecinkowych || Added support for floating point numbers
    -Naprawienie błędów które pozwalały na klikanie kilka razy operacji matematycznych co wyrzucało aplikacje || Fixed bugs that allowed clicking math operations several times which crashed applications
    -Dodanie przycisku "," || Adding "," button
    -Dodanie nowych klas || Adding new classes

---
+++ Calc/Properties/Commands/RelayCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calc.Properties.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+        private ICommand resultCommand;
+
+        public RelayCommand(Action<object> execute)
+            :this(execute, null)
+        {
+
+        }
+
+        public RelayCommand(Action<object> execute, 
+            Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new
+                ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(ICommand resultCommand)
+        {
+            this.resultCommand = resultCommand;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute
+                (parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested += value;
+            }
+        }
+
+        public void Execute (object parameter)
+        {
+            _execute(parameter);
+        }
+       
+    }
+}
---
+++ Calc/Properties/MainViewModel.cs
@@ -0,0 +1,102 @@
+﻿using Calc.Properties.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Calc.Properties
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+
+        private string _screenValue;
+        private List<string> _availibleOperations = new List<string> { "+", "-", "/", "*" };
+        private DataTable _dataTable = new DataTable();
+        private bool _isLastOperation;
+        
+        public MainViewModel()
+        {
+            ScreenValue = "0";
+            AddNumberCommand = new RelayCommand(AddNumber);
+            AddOperationCommand = new RelayCommand(AddOperation, CanAddOperation);
+            ResultCommand = new RelayCommand(ResultOperation, CanGetResult);
+            ClearCommand = new RelayCommand(ClearOperation);
+        }
+
+        private bool CanGetResult(object obj) => !_isLastOperation;
+
+        private bool CanAddOperation(object obj) => !_isLastOperation;
+
+        private void ClearOperation(object obj)
+        {
+            ScreenValue = "0";
+
+            _isLastOperation = false;
+        }
+
+        private void ResultOperation(object obj)
+        {
+            var result = Math.Round(Convert.ToDouble((_dataTable.Compute(ScreenValue.Replace(",", "."), ""))), 2);
+
+            ScreenValue = result.ToString();
+        }
+
+        private void AddOperation(object obj)
+        {
+            var operation = obj as string;
+
+            ScreenValue += operation;
+
+            _isLastOperation = true;
+        }
+
+        private void AddNumber(object obj)
+        {
+            var number = obj as string;
+
+            if(ScreenValue=="0" && number != ",")
+            {
+                ScreenValue = string.Empty;
+            }    
+            else if (number == "," && _availibleOperations.Contains(ScreenValue.Substring(ScreenValue.Length-1)))
+            {
+                number = "0,";
+            }
+            ScreenValue += number;
+
+            _isLastOperation = false;
+        }
+
+        public ICommand AddNumberCommand { get; set; }
+        public ICommand AddOperationCommand { get; set; }
+        public ICommand ResultCommand { get; set; }
+        public ICommand ClearCommand { get; set; }
+
+
+        public string ScreenValue
+        {
+            get { return _screenValue; }
+            set { 
+                _screenValue = value;
+                OnPropertyChanged();
+                }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new
+                PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
